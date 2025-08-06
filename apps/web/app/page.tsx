7e6d5d7c63d3f5564d671853223ad45bae0bdc0d
Navesh Jaiswal <navesh.professional@gmail.com>
2025-08-06 12:10:49 +0530

    client page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,76 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+"use client";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+import { useSocket } from "../context/SocketProvider";
+import classes from "./page.module.css";
+import { useState, useEffect, useRef } from "react";
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+export default function Page() {
+  const { sendMessage, messages } = useSocket();
+  const [message, setMessage] = useState("");
+
+  const boxRef = useRef<HTMLDivElement | null>(null);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const box = boxRef.current;
+    const bottom = bottomRef.current;
+    if (!box || !bottom) return;
+
+    const isNearBottom =
+      box.scrollHeight - box.scrollTop - box.clientHeight < 100;
+
+    if (isNearBottom) {
+      requestAnimationFrame(() => {
+        bottom.scrollIntoView({ behavior: "smooth" });
+      });
+    }
+  }, [messages]);
 
   return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+    <div className={classes.container}>
+      <h1 className={classes.logo}>SYNTREA</h1>
+      <p className={classes.description}>
+        Real-time chat application powered by WebSockets and Valkey.
+      </p>
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
+      <div className={classes["message-box"]} ref={boxRef}>
+        {messages.map((e, i) => (
+          <li key={i}>{e}</li>
+        ))}
+        <div ref={bottomRef} />
+      </div>
+
+      <div className={classes["input-area"]}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              const trimmed = message.trim();
+              if (trimmed !== "") {
+                sendMessage(trimmed);
+                setMessage("");
+              }
+            }
+          }}
+          type="text"
+          className={classes["chat-input"]}
+          placeholder="Enter message.."
         />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.com/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            const trimmed = message.trim();
+            if (trimmed !== "") {
+              sendMessage(trimmed);
+              setMessage("");
+            }
+          }}
+          className={classes["button"]}
+          type="button"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.com?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.com →
-        </a>
-      </footer>
+          ↑
+        </button>
+      </div>
     </div>
   );
 }
